70d4aa83f5c8947f5486de8eec7212fa28094dba
renyellen machado <101356078+renyellen@users.noreply.github.com>
2022-08-04 17:59:32 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class app extends Component {
+  render() {
+    return (
+      <div>
+        <p>Oi, mundo de novo! </p>
+      </div>
+    );
+  }
+}
